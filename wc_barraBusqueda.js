1c1c641049632f70efb5fdde51286d06d283d5d3
Viktor Carranza <viktor@DebVk.lan>
2018-04-17 02:44:50 -0600

    Sin poder llamar los eventos dentro de la clase :C

--- wc_barraBusqueda.js
+++ wc_barraBusqueda.js
@@ -1,6 +1,8 @@
 class Busqueda extends HTMLElement {
     constructor(){
         super();
+      //  this.importDocument = document.currentScript.ownerDocument;
+      this.input = this.input;
     }
     connectedCallback(){
         let shadowRoot = this.attachShadow({mode:'open'});
@@ -10,8 +12,49 @@ class Busqueda extends HTMLElement {
             <ul id="autocomplete-results">
             </ul>
         </div>
-        `
-    }
+        `;
+        this.input = document.querySelector('input');
+    };
+
+
+    autocomplete(val) {
+        var people_return = [];
+ 
+        for (i = 0; i < soloArreglo.length; i++) {
+            if (val === soloArreglo[i].slice(0, val.length)) {
+            people_return.push(soloArreglo[i]);
+            }
+        }
+    return people_return;
+    };
+ 
+ // events
+ 
+ input.onkeyup = function(e) {
+   input_val = this.value; // updates the variable on each ocurrence
+ 
+   if (input_val.length > 0) {
+     var people_to_show = [];
+ 
+     autocomplete_results = document.getElementById("autocomplete-results");
+     autocomplete_results.innerHTML = '';
+     people_to_show = autocomplete(input_val);
+     
+     for (i = 0; i < people_to_show.length; i++) {
+       autocomplete_results.innerHTML += '<li>' + people_to_show[i] + '</li>';
+ 
+     }
+     autocomplete_results.style.display = 'block';
+   } else {
+     people_to_show = [];
+     autocomplete_results.innerHTML = '';
+   }
+ }
 }
+
+
+
+
+
 
 window.customElements.define('webcomp-busqueda', Busqueda);
