47f02ae3603fbdfd219481d3cdf5ec2ea6c86bf1
Viktor Carranza <viktor@DebVk.lan>
2018-04-17 04:14:47 -0600

    metí todo dentro del shadow Dom para no tener que estar migrando codigo de DOM a DOM no hace la llamada de los metodos desde afuera

--- wc_barraBusqueda.js
+++ wc_barraBusqueda.js
@@ -2,7 +2,9 @@ class Busqueda extends HTMLElement {
     constructor(){
         super();
       //  this.importDocument = document.currentScript.ownerDocument;
-      this.input = this.input;
+      let input = this.input;
+      //let autoComp = this.autoComp;
+      let autocomplete_results = this.autocomplete_results;
     }
     connectedCallback(){
         let shadowRoot = this.attachShadow({mode:'open'});
@@ -12,43 +14,104 @@ class Busqueda extends HTMLElement {
             <ul id="autocomplete-results">
             </ul>
         </div>
-        `;
-        this.input = document.querySelector('input');
-    };
 
+        <script>
 
-    autocomplete(val) {
-        var people_return = [];
- 
-        for (i = 0; i < soloArreglo.length; i++) {
-            if (val === soloArreglo[i].slice(0, val.length)) {
-            people_return.push(soloArreglo[i]);
+        let datajson = []; //arreglo de objetos obtenidos del json
+        let soloArreglo = []; //arreglo con los nombres a sugerir en el buscador
+        
+        function obtenerPath(ruta){
+            fetch(ruta)  //Realiza la peticion
+                .then(r => r.json()) //convierte el response de la promesa en tipo json
+                .then(data =>{
+                    datajson = data; // ahora lo manda al arreglo datajson
+                }) 
+                .catch(e => console.error('Algo salio mal')); //en caso de error en el servidor
             }
+        
+        function cumpliendoPromesa(extra){ //extra en caso de ser matriz
+            setTimeout(()=>{ //se le hace espera para la respuesta del servidor
+                if(extra!= null){
+                console.log("tiempo 0.5seg");
+                    for(let i=0; i<10;i++){
+                        soloArreglo[i] = datajson[i][extra]; //se llena el arreglo con los nombres a sugerir
+                    }
+                }else{
+                    for(let i=0; i<10;i++){
+                        soloArreglo[i] = datajson[i]; //se llena el arreglo con los nombres a sugerir
+                    }
+                }
+            }, 500);
         }
-    return people_return;
+
+
+
+
+
+
+        obtenerPath('https://jsonplaceholder.typicode.com/users');
+        cumpliendoPromesa('name');
+
+
+
+
+
+
+
+
+
+
+        var input = document.querySelector('input');
+        autocomplete(val) {
+            var people_return = [];
+     
+            for (let i = 0; i < soloArreglo.length; i++) {
+                if (val === soloArreglo[0].slice(0, val.length)) {
+                people_return.push(soloArreglo[0]);
+                }
+            }
+        return people_return;
+        };
+
+
+        input.onkeyup = function(e) {
+            input_val = this.value; // updates the variable on each ocurrence
+          
+            if (input_val.length > 0) {
+              var people_to_show = [];
+          
+              autocomplete_results = document.getElementById("autocomplete-results");
+              autocomplete_results.innerHTML = '';
+              people_to_show = autocomplete(input_val);
+              
+              for (i = 0; i < people_to_show.length; i++) {
+                autocomplete_results.innerHTML += '<li>' + people_to_show[i] + '</li>';
+          
+              }
+              autocomplete_results.style.display = 'block';
+            } else {
+              people_to_show = [];
+              autocomplete_results.innerHTML = '';
+            }
+          }
+        </script>
+
+
+        
+        `;
+ 
+
+
+
+
+
     };
+
+
  
  // events
- evento(){
-        input.onkeyup = function(e) {
-        input_val = this.value; // updates the variable on each ocurrence
-            if (input_val.length > 0) {
-                var people_to_show = [];
-            
-                autocomplete_results = document.getElementById("autocomplete-results");
-                autocomplete_results.innerHTML = '';
-                people_to_show = autocomplete(input_val);
-                
-                for (i = 0; i < people_to_show.length; i++) {
-                autocomplete_results.innerHTML += '<li>' + people_to_show[i] + '</li>';
-            
-                }
-                autocomplete_results.style.display = 'block';
-            } else {
-                people_to_show = [];
-                autocomplete_results.innerHTML = '';
-            }
-        }
+ evento(input){
+
     }
 }
 
